de0b229384d9c59455f8ac2e95aa804990cb5c6d
ElioH76 <102657535+ElioH76@users.noreply.github.com>
2022-06-01 23:53:56 +0200

    Add files via upload

---
+++ CalculatriceVB/Form1.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.textCalc = New System.Windows.Forms.TextBox()
+        Me.bout_0 = New System.Windows.Forms.Button()
+        Me.bout_1 = New System.Windows.Forms.Button()
+        Me.bout_2 = New System.Windows.Forms.Button()
+        Me.bout_3 = New System.Windows.Forms.Button()
+        Me.bout_6 = New System.Windows.Forms.Button()
+        Me.bout_5 = New System.Windows.Forms.Button()
+        Me.bout_4 = New System.Windows.Forms.Button()
+        Me.bout_7 = New System.Windows.Forms.Button()
+        Me.bout_8 = New System.Windows.Forms.Button()
+        Me.bout_9 = New System.Windows.Forms.Button()
+        Me.bout_sous = New System.Windows.Forms.Button()
+        Me.bout_egal = New System.Windows.Forms.Button()
+        Me.bout_add = New System.Windows.Forms.Button()
+        Me.bout_div = New System.Windows.Forms.Button()
+        Me.bout_mult = New System.Windows.Forms.Button()
+        Me.bout_reset = New System.Windows.Forms.Button()
+        Me.bout_virg = New System.Windows.Forms.Button()
+        Me.bout_neg = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'textCalc
+        '
+        Me.textCalc.Enabled = False
+        Me.textCalc.Location = New System.Drawing.Point(98, 100)
+        Me.textCalc.Multiline = True
+        Me.textCalc.Name = "textCalc"
+        Me.textCalc.Size = New System.Drawing.Size(342, 71)
+        Me.textCalc.TabIndex = 10
+        '
+        'bout_0
+        '
+        Me.bout_0.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_0.Location = New System.Drawing.Point(185, 391)
+        Me.bout_0.Name = "bout_0"
+        Me.bout_0.Size = New System.Drawing.Size(81, 58)
+        Me.bout_0.TabIndex = 11
+        Me.bout_0.Text = "0"
+        Me.bout_0.UseVisualStyleBackColor = True
+        '
+        'bout_1
+        '
+        Me.bout_1.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_1.Location = New System.Drawing.Point(98, 327)
+        Me.bout_1.Name = "bout_1"
+        Me.bout_1.Size = New System.Drawing.Size(81, 58)
+        Me.bout_1.TabIndex = 12
+        Me.bout_1.Text = "1"
+        Me.bout_1.UseVisualStyleBackColor = True
+        '
+        'bout_2
+        '
+        Me.bout_2.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_2.Location = New System.Drawing.Point(185, 327)
+        Me.bout_2.Name = "bout_2"
+        Me.bout_2.Size = New System.Drawing.Size(81, 58)
+        Me.bout_2.TabIndex = 13
+        Me.bout_2.Text = "2"
+        Me.bout_2.UseVisualStyleBackColor = True
+        '
+        'bout_3
+        '
+        Me.bout_3.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_3.Location = New System.Drawing.Point(272, 327)
+        Me.bout_3.Name = "bout_3"
+        Me.bout_3.Size = New System.Drawing.Size(81, 58)
+        Me.bout_3.TabIndex = 14
+        Me.bout_3.Text = "3"
+        Me.bout_3.UseVisualStyleBackColor = True
+        '
+        'bout_6
+        '
+        Me.bout_6.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_6.Location = New System.Drawing.Point(272, 263)
+        Me.bout_6.Name = "bout_6"
+        Me.bout_6.Size = New System.Drawing.Size(81, 58)
+        Me.bout_6.TabIndex = 15
+        Me.bout_6.Text = "6"
+        Me.bout_6.UseVisualStyleBackColor = True
+        '
+        'bout_5
+        '
+        Me.bout_5.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_5.Location = New System.Drawing.Point(185, 263)
+        Me.bout_5.Name = "bout_5"
+        Me.bout_5.Size = New System.Drawing.Size(81, 58)
+        Me.bout_5.TabIndex = 16
+        Me.bout_5.Text = "5"
+        Me.bout_5.UseVisualStyleBackColor = True
+        '
+        'bout_4
+        '
+        Me.bout_4.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_4.Location = New System.Drawing.Point(98, 263)
+        Me.bout_4.Name = "bout_4"
+        Me.bout_4.Size = New System.Drawing.Size(81, 58)
+        Me.bout_4.TabIndex = 17
+        Me.bout_4.Text = "4"
+        Me.bout_4.UseVisualStyleBackColor = True
+        '
+        'bout_7
+        '
+        Me.bout_7.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_7.Location = New System.Drawing.Point(98, 199)
+        Me.bout_7.Name = "bout_7"
+        Me.bout_7.Size = New System.Drawing.Size(81, 58)
+        Me.bout_7.TabIndex = 18
+        Me.bout_7.Text = "7"
+        Me.bout_7.UseVisualStyleBackColor = True
+        '
+        'bout_8
+        '
+        Me.bout_8.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_8.Location = New System.Drawing.Point(185, 199)
+        Me.bout_8.Name = "bout_8"
+        Me.bout_8.Size = New System.Drawing.Size(81, 58)
+        Me.bout_8.TabIndex = 19
+        Me.bout_8.Text = "8"
+        Me.bout_8.UseVisualStyleBackColor = True
+        '
+        'bout_9
+        '
+        Me.bout_9.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_9.Location = New System.Drawing.Point(272, 199)
+        Me.bout_9.Name = "bout_9"
+        Me.bout_9.Size = New System.Drawing.Size(81, 58)
+        Me.bout_9.TabIndex = 20
+        Me.bout_9.Text = "9"
+        Me.bout_9.UseVisualStyleBackColor = True
+        '
+        'bout_sous
+        '
+        Me.bout_sous.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_sous.Location = New System.Drawing.Point(446, 263)
+        Me.bout_sous.Name = "bout_sous"
+        Me.bout_sous.Size = New System.Drawing.Size(81, 58)
+        Me.bout_sous.TabIndex = 21
+        Me.bout_sous.Text = "-"
+        Me.bout_sous.UseVisualStyleBackColor = True
+        '
+        'bout_egal
+        '
+        Me.bout_egal.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_egal.Location = New System.Drawing.Point(359, 391)
+        Me.bout_egal.Name = "bout_egal"
+        Me.bout_egal.Size = New System.Drawing.Size(81, 58)
+        Me.bout_egal.TabIndex = 22
+        Me.bout_egal.Text = "="
+        Me.bout_egal.UseVisualStyleBackColor = True
+        '
+        'bout_add
+        '
+        Me.bout_add.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_add.Location = New System.Drawing.Point(359, 327)
+        Me.bout_add.Name = "bout_add"
+        Me.bout_add.Size = New System.Drawing.Size(81, 58)
+        Me.bout_add.TabIndex = 23
+        Me.bout_add.Text = "+"
+        Me.bout_add.UseVisualStyleBackColor = True
+        '
+        'bout_div
+        '
+        Me.bout_div.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_div.Location = New System.Drawing.Point(359, 263)
+        Me.bout_div.Name = "bout_div"
+        Me.bout_div.Size = New System.Drawing.Size(81, 58)
+        Me.bout_div.TabIndex = 24
+        Me.bout_div.Text = "/"
+        Me.bout_div.UseVisualStyleBackColor = True
+        '
+        'bout_mult
+        '
+        Me.bout_mult.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_mult.Location = New System.Drawing.Point(359, 199)
+        Me.bout_mult.Name = "bout_mult"
+        Me.bout_mult.Size = New System.Drawing.Size(81, 58)
+        Me.bout_mult.TabIndex = 25
+        Me.bout_mult.Text = "X"
+        Me.bout_mult.UseVisualStyleBackColor = True
+        '
+        'bout_reset
+        '
+        Me.bout_reset.Location = New System.Drawing.Point(446, 199)
+        Me.bout_reset.Name = "bout_reset"
+        Me.bout_reset.Size = New System.Drawing.Size(94, 58)
+        Me.bout_reset.TabIndex = 26
+        Me.bout_reset.Text = "Reset"
+        Me.bout_reset.UseVisualStyleBackColor = True
+        '
+        'bout_virg
+        '
+        Me.bout_virg.Location = New System.Drawing.Point(98, 391)
+        Me.bout_virg.Name = "bout_virg"
+        Me.bout_virg.Size = New System.Drawing.Size(81, 58)
+        Me.bout_virg.TabIndex = 27
+        Me.bout_virg.Text = ","
+        Me.bout_virg.UseVisualStyleBackColor = True
+        '
+        'bout_neg
+        '
+        Me.bout_neg.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bout_neg.Location = New System.Drawing.Point(272, 391)
+        Me.bout_neg.Name = "bout_neg"
+        Me.bout_neg.Size = New System.Drawing.Size(81, 58)
+        Me.bout_neg.TabIndex = 28
+        Me.bout_neg.Text = "(-)"
+        Me.bout_neg.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(577, 596)
+        Me.Controls.Add(Me.bout_neg)
+        Me.Controls.Add(Me.bout_virg)
+        Me.Controls.Add(Me.bout_reset)
+        Me.Controls.Add(Me.bout_mult)
+        Me.Controls.Add(Me.bout_div)
+        Me.Controls.Add(Me.bout_add)
+        Me.Controls.Add(Me.bout_egal)
+        Me.Controls.Add(Me.bout_sous)
+        Me.Controls.Add(Me.bout_9)
+        Me.Controls.Add(Me.bout_8)
+        Me.Controls.Add(Me.bout_7)
+        Me.Controls.Add(Me.bout_4)
+        Me.Controls.Add(Me.bout_5)
+        Me.Controls.Add(Me.bout_6)
+        Me.Controls.Add(Me.bout_3)
+        Me.Controls.Add(Me.bout_2)
+        Me.Controls.Add(Me.bout_1)
+        Me.Controls.Add(Me.bout_0)
+        Me.Controls.Add(Me.textCalc)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents textCalc As TextBox
+    Friend WithEvents bout_0 As Button
+    Friend WithEvents bout_1 As Button
+    Friend WithEvents bout_2 As Button
+    Friend WithEvents bout_3 As Button
+    Friend WithEvents bout_6 As Button
+    Friend WithEvents bout_5 As Button
+    Friend WithEvents bout_4 As Button
+    Friend WithEvents bout_7 As Button
+    Friend WithEvents bout_8 As Button
+    Friend WithEvents bout_9 As Button
+    Friend WithEvents bout_sous As Button
+    Friend WithEvents bout_egal As Button
+    Friend WithEvents bout_add As Button
+    Friend WithEvents bout_div As Button
+    Friend WithEvents bout_mult As Button
+    Friend WithEvents bout_reset As Button
+    Friend WithEvents bout_virg As Button
+    Friend WithEvents bout_neg As Button
+End Class
---
+++ CalculatriceVB/Form1.vb
@@ -0,0 +1,150 @@
+﻿Public Class Form1
+
+    Dim operateur = ""
+    Dim operande1 As Double
+    Dim operande2 As Double
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub textCalc_TextChanged(sender As Object, e As EventArgs) Handles textCalc.TextChanged
+
+    End Sub
+
+    Private Sub bout_0_Click(sender As Object, e As EventArgs) Handles bout_0.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "0"
+        Else
+            textCalc.Text = textCalc.Text + "0"
+        End If
+    End Sub
+    Private Sub bout_1_Click(sender As Object, e As EventArgs) Handles bout_1.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "1"
+        Else
+            textCalc.Text = textCalc.Text + "1"
+        End If
+    End Sub
+    Private Sub bout_2_Click(sender As Object, e As EventArgs) Handles bout_2.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "2"
+        Else
+            textCalc.Text = textCalc.Text + "2"
+        End If
+    End Sub
+    Private Sub bout_3_Click(sender As Object, e As EventArgs) Handles bout_3.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "3"
+        Else
+            textCalc.Text = textCalc.Text + "3"
+        End If
+    End Sub
+    Private Sub bout_4_Click(sender As Object, e As EventArgs) Handles bout_4.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "4"
+        Else
+            textCalc.Text = textCalc.Text + "4"
+        End If
+    End Sub
+    Private Sub bout_5_Click(sender As Object, e As EventArgs) Handles bout_5.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "5"
+        Else
+            textCalc.Text = textCalc.Text + "5"
+        End If
+    End Sub
+    Private Sub bout_6_Click(sender As Object, e As EventArgs) Handles bout_6.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "6"
+        Else
+            textCalc.Text = textCalc.Text + "6"
+        End If
+    End Sub
+    Private Sub bout_7_Click(sender As Object, e As EventArgs) Handles bout_7.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "7"
+        Else
+            textCalc.Text = textCalc.Text + "7"
+        End If
+    End Sub
+    Private Sub bout_8_Click(sender As Object, e As EventArgs) Handles bout_8.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "8"
+        Else
+            textCalc.Text = textCalc.Text + "8"
+        End If
+    End Sub
+    Private Sub bout_9_Click(sender As Object, e As EventArgs) Handles bout_9.Click
+        If textCalc.Text = "0" Then
+            textCalc.Text = "9"
+        Else
+            textCalc.Text = textCalc.Text + "9"
+        End If
+    End Sub
+
+    Private Sub bout_egal_Click(sender As Object, e As EventArgs) Handles bout_egal.Click
+        operande2 = textCalc.Text
+
+        If operateur = "+" Then
+            textCalc.Text = operande1 + operande2
+        ElseIf operateur = "-" Then
+            textCalc.Text = operande1 - operande2
+        ElseIf operateur = "/" Then
+            textCalc.Text = operande1 / operande2
+        ElseIf operateur = "x" Then
+            textCalc.Text = operande1 * operande2
+        End If
+
+
+    End Sub
+
+    Private Sub bout_add_Click(sender As Object, e As EventArgs) Handles bout_add.Click
+        operateur = "+"
+        operande1 = textCalc.Text
+        textCalc.Text = " "
+
+    End Sub
+
+    Private Sub bout_div_Click(sender As Object, e As EventArgs) Handles bout_div.Click
+        operateur = "/"
+        operande1 = textCalc.Text
+        textCalc.Text = " "
+    End Sub
+
+    Private Sub bout_mult_Click(sender As Object, e As EventArgs) Handles bout_mult.Click
+        operateur = "x"
+        operande1 = textCalc.Text
+        textCalc.Text = " "
+    End Sub
+
+    Private Sub bout_sous_Click(sender As Object, e As EventArgs) Handles bout_sous.Click
+
+        operateur = "-"
+        operande1 = textCalc.Text
+        textCalc.Text = " "
+
+    End Sub
+
+    Private Sub bout_reset_Click(sender As Object, e As EventArgs) Handles bout_reset.Click
+        textCalc.Text = " "
+        operande1 = 0
+        operande2 = 0
+    End Sub
+
+    Private Sub bout_virg_Click(sender As Object, e As EventArgs) Handles bout_virg.Click
+        textCalc.Text = textCalc.Text + ","
+
+    End Sub
+
+    Private Sub bout_neg_Click(sender As Object, e As EventArgs) Handles bout_neg.Click
+
+        If operande1 = 0 Or operande2 = 0 Then
+            textCalc.Text = "-"
+        Else
+            operande1 = textCalc.Text
+            textCalc.Text = " "
+        End If
+    End Sub
+End Class
